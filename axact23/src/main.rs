36cd870736d901b02274979ddb0e9cf0017d8c9f
V4yukon <yxyvan@gamil.com>
2023-06-20 23:33:56 +0800

    hhhh, I like it so much

--- axact23/src/main.rs
+++ axact23/src/main.rs
@@ -1,9 +1,13 @@
-use axum::{Router, Server, routing::get, extract::State};
-use sysinfo::{System, SystemExt, RefreshKind, CpuRefreshKind, CpuExt};
+use axum::{Router, Server, routing::get, extract::State, response::{IntoResponse, Html}, Json, http::Response};
+use sysinfo::{System, SystemExt, CpuExt};
 use std::sync::{Arc, Mutex};
 #[tokio::main]
 async fn main() {
-    let router = Router::new().route("/", get(root_get)).with_state(AppState {sys: Arc::new(Mutex::new(System::new()))});
+    let router = Router::new()
+    .route("/", get(root_get))
+    .route("/index.js", get(indexjs_get))
+    .route("/api/cpus", get(get_cpus))
+    .with_state(AppState {sys: Arc::new(Mutex::new(System::new()))});
 
 
     let server = Server::bind(&"0.0.0.0:8032".parse().unwrap()).serve(router.into_make_service());
@@ -20,21 +24,39 @@ async fn main() {
 struct AppState {
     sys: Arc<Mutex<System>>,
 }
-async fn root_get(State(state): State<AppState>) -> String {
-    use std::fmt::Write;
-    let mut s = String::new();
+#[axum::debug_handler]
+async fn root_get() -> impl IntoResponse {
+    let markup = tokio::fs::read_to_string("src/index.html").await.unwrap();
+    Html(markup)
+    
+}
+async fn indexjs_get() -> impl IntoResponse {
+    let markup = tokio::fs::read_to_string("src/index.js").await.unwrap();
+    Response::builder()
+    .status(200)
+    .header("content-type", "application/javascript;charset=utf-8")
+    .body(markup)
+    .unwrap()
+}
+#[axum::debug_handler]
+async fn get_cpus(State(state): State<AppState>) -> impl IntoResponse {
+    
+    
 
     let mut sys = state.sys.lock().unwrap();
     //let mut sys = System::new_with_specifics(RefreshKind::new().with_cpu(CpuRefreshKind::everything(),));
     sys.refresh_cpu();
 
-    for (i, cpu) in sys.cpus().iter().enumerate() {
-        let i = i + 1;
-        let usage = cpu.cpu_usage();
+    let v: Vec<_> = sys.cpus().iter().map(|cpu| cpu.cpu_usage()).collect();
+    Json(v)
 
-        writeln!(&mut s, "CPU {i} {usage}%").unwrap();
-    }
+    // for (i, cpu) in sys.cpus().iter().enumerate() {
+    //     let i = i + 1;
+    //     let usage = cpu.cpu_usage();
 
-    s
+    //     writeln!(&mut s, "CPU {i} {usage}%").unwrap();
+    // }
+
+    
 
 }
