a84ec479d6697ed4093cbcacb035944cb2fe7dd5
V4yukon <yxyvan@gmail.com>
2023-07-11 23:00:43 +0800

    update another sql

--- movies/api/shuttle/src/main.rs
+++ movies/api/shuttle/src/main.rs
@@ -21,3 +21,15 @@ async fn actix_web(
 
     Ok(config.into())
 }
+#[get("/version")]
+async fn version(db: actix_web::web::Data<sqlx::PgPool>) -> String {
+    let result: Result<String, sqlx::Error> = sqlx::query_scalar("SELECT
+    version()")
+    .fetch_one(db.get_ref())
+    .await;
+
+    match result {
+        Ok(version) => version,
+        Err(e) => format!("Error: {:?}", e),
+    }
+}
